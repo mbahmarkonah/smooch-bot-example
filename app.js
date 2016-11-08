c1f791c1baed7f45373329d220cb733bbf14aebd
mbahmarkonah <mbah.markonah@gmail.com>
2016-11-09 06:27:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 var favicon = require('serve-favicon');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function(req, res) {
     res.render('index', {
