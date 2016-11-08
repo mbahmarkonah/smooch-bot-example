671e7f3822b4fd76759f511fc28075e54941c8f9
mbahmarkonah <mbah.markonah@gmail.com>
2016-11-09 06:22:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
+var favicon = require('serve-favicon');
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 app.get('/', function(req, res) {
     res.render('index', {
         appToken: process.env.SMOOCH_APP_TOKEN
